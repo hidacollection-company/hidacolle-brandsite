7d9f8eb9385046e07268cf7ee83b20f08098de75
tairahoraguchi <>
2023-02-16 12:24:13 +0900

    Add Google tag manager, Sitemap and robot.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://hidacolle.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  outDir: './out',
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,20 @@ import { AppProps } from 'next/app'
 import Head from 'next/head'
 import 'sanitize.css'
 import '../styles/scss/style.scss'
-import Layout from '../components/Layout';
+import Layout from '../components/Layout'
 
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <>
-    <Head>
-      <title />
-    </Head>
-    <Layout>
-        <Component {...pageProps} />
-    </Layout>
-  </>
+
+    <>
+      <Head>
+        <title />
+      </Head>
+      <Layout>
+          <Component {...pageProps} />
+      </Layout>
+    </>
+
 )
 
 export default App
