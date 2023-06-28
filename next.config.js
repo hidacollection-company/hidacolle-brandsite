ae7f06738340f68f6467447cd8a797f47fb9828a
tairahoraguchi <>
2023-06-28 22:41:56 +0900

    Update

--- next.config.js
+++ next.config.js
@@ -24,28 +24,11 @@ loadEnv(process.env.APP_ENV);
 /**
  * @param {string} appEnv
  */
-
-// function loadEnv(appEnv = "local") {
-//   const env = {
-//     ...require(`./env/env.${appEnv}`),
-//     NEXT_PUBLIC_APP_ENV: appEnv,
-//   };
-
-//   Object.entries(env).forEach(([key, value]) => {
-//     process.env[key] = value;
-//   });
-// }
-
 function loadEnv(appEnv = "local") {
-  let env;
-  try {
-    env = require(`./env/env.${appEnv}`);
-  } catch (error) {
-    console.error(`Failed to load environment file for appEnv: ${appEnv}`);
-    process.exit(1);
-  }
-
-  // 以下の処理は変更の必要はありません
+  const env = {
+    ...require(`./env/env.${appEnv}`),
+    NEXT_PUBLIC_APP_ENV: appEnv,
+  };
 
   Object.entries(env).forEach(([key, value]) => {
     process.env[key] = value;
