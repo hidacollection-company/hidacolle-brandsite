87aed782ced8c0114564e6d7c1da0ab8bdf8cb52
tairahoraguchi <>
2023-06-28 22:28:07 +0900

    uodate

--- next.config.js
+++ next.config.js
@@ -24,11 +24,28 @@ loadEnv(process.env.APP_ENV);
 /**
  * @param {string} appEnv
  */
+
+// function loadEnv(appEnv = "local") {
+//   const env = {
+//     ...require(`./env/env.${appEnv}`),
+//     NEXT_PUBLIC_APP_ENV: appEnv,
+//   };
+
+//   Object.entries(env).forEach(([key, value]) => {
+//     process.env[key] = value;
+//   });
+// }
+
 function loadEnv(appEnv = "local") {
-  const env = {
-    ...require(`./env/env.${appEnv}`),
-    NEXT_PUBLIC_APP_ENV: appEnv,
-  };
+  let env;
+  try {
+    env = require(`./env/env.${appEnv}`);
+  } catch (error) {
+    console.error(`Failed to load environment file for appEnv: ${appEnv}`);
+    process.exit(1);
+  }
+
+  // 以下の処理は変更の必要はありません
 
   Object.entries(env).forEach(([key, value]) => {
     process.env[key] = value;
